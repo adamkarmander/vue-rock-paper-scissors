89e7d26eaa2161314f39a4e807cc515b167d5039
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2022-04-23 22:42:42 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Score userScore="0" computerScore="0"/>
+</template>
+
+<script>
+import Score from './components/Score.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Score
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+  #app {
+    text-align: center;
+    font-family: 'Open Sans', sans-serif;
+  }
+</style>
---
+++ src/components/Score.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Score: {{userScore}}-{{computerScore}} (You-Computer)</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Score',
+  props: {
+    userScore: {
+      type: Number,
+      required: true
+    },
+    computerScore: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
